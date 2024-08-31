e7aa51b712ee4f76e6c354cf4dc4d0f37b751118
Parsa Asgari <parsa.asgari3264@gmail.com>
2024-08-31 21:43:40 +0000

    added collapsible sidebar

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/layout/header';
-import Sidebar from '@/components/layout/sidebar';
+// import Sidebar from '@/components/layout/sidebar';
+import AdminPanelLayout from '@/components/collapsible_sidebar/admin-panel-layout';
 import { ScrollArea } from '@/components/ui/scroll-area';
 import type { Metadata } from 'next';
 
@@ -14,12 +15,10 @@ export default function DashboardLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div className="flex">
-      <Sidebar />
-      <main className="w-full flex-1 overflow-hidden">
-        <Header />
-        {children}
-      </main>
-    </div>
+    <AdminPanelLayout>
+      <Header />
+
+      {children}
+    </AdminPanelLayout>
   );
 }
